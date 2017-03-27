fdde2ec91d6bff3fe69a881c30f561778dbcfbcd
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-03-27 01:08:22 -0300

    Se agregan creadores de acciones

--- app/actions/panel.js
+++ app/actions/panel.js
@@ -1,2 +1,16 @@
 export const AGREGAR_PEDIDO = "AGREGAR_PEDIDO";
 export const ELIMINAR_PEDIDO = "ELIMINAR_PEDIDO";
+
+export function agregarPedido(pedido) {
+  return {
+    type: AGREGAR_PEDIDO,
+    pedido
+  };
+}
+
+export function eliminarPedido(indice) {
+  return {
+    type: ELIMINAR_PEDIDO,
+    indice
+  };
+}
