a66a98725f2edfd02d95d5e3a971ec59aeaaf332
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-03-27 01:11:26 -0300

    Se edita estado

--- app/reducers/panel.js
+++ app/reducers/panel.js
@@ -3,9 +3,15 @@ import { AGREGAR_PEDIDO, ELIMINAR_PEDIDO } from "../actions/panel";
 export default function panel(state = [], action) {
   switch (action.type) {
     case AGREGAR_PEDIDO:
-      return state;
+      return [
+        ...state,
+        action.pedido
+      ];
     case ELIMINAR_PEDIDO:
-      return state;
+      return [
+        ...state.slice(0, action.indice),
+        ...state.slice(action.indice + 1)
+      ];
     default:
       return state;
   }
