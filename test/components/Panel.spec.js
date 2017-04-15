1303164eca4c8e0cea8629313a4486e7449fcf1e
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-04-15 18:20:32 -0300

    Se formatea prueba de Panel

--- test/components/Panel.spec.js
+++ test/components/Panel.spec.js
@@ -2,7 +2,7 @@ import { shallow } from "enzyme";
 import React from "react";
 import Panel from "../../app/components/Panel";
 
-function setup() {
+function crearPanel() {
   const props = {
     pedidos: [{
       id: 1,
@@ -11,21 +11,22 @@ function setup() {
       id: 2,
       nombre: "Pedido 2"
     }],
-    subscribirCambiosPanel: () => { }
+    subscribirCambiosPanel: jest.fn()
   };
 
   const panelEnzyme = shallow(<Panel {...props} />);
 
-  return {
-    panelEnzyme
-  };
+  return panelEnzyme;
 }
 
 describe("Componenete Panel", () => {
   it("Debe renderizar componente", () => {
-    const { panelEnzyme } = setup();
+    const panelEnzyme = crearPanel();
     expect(panelEnzyme.find("h1").text())
       .toBe("Panel pedidos");
+  });
+  it("La cantidad de elementos en la liste debe ser igual a los pedidos ", () => {
+    const panelEnzyme = crearPanel();
     expect(panelEnzyme.find("li").length)
       .toBe(2);
   });
