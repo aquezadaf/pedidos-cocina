422d7961bfc2b16b124a7df29a5de8b895ead82e
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-04-17 14:26:33 -0300

    Se actualiza prueba de panel para reflejar el nuevo componente agregado

--- test/components/Panel.spec.js
+++ test/components/Panel.spec.js
@@ -27,7 +27,7 @@ describe("Componenete Panel", () => {
   });
   it("La cantidad de elementos en la liste debe ser igual a los pedidos ", () => {
     const panelEnzyme = crearPanel();
-    expect(panelEnzyme.find("div > div > div").length)
+    expect(panelEnzyme.find("Pedido").length)
       .toBe(2);
   });
 });
