bd0e8e47da132d090a448698360b40cdafa776e5
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-04-19 23:35:12 -0300

    Se agregan pruebas para el ordenamiento de pedidos segun prioridad

--- test/components/Panel.spec.js
+++ test/components/Panel.spec.js
@@ -6,28 +6,57 @@ function crearPanel() {
   const props = {
     pedidos: [{
       id: 1,
-      nombre: "Pedido 1"
+      nombre: "Pedido 1",
+      prioridad: 1
     }, {
       id: 2,
-      nombre: "Pedido 2"
+      nombre: "Pedido 2",
+      prioridad: 2
     }],
     subscribirCambiosPanel: jest.fn()
   };
 
   const panelEnzyme = shallow(<Panel {...props} />);
 
-  return panelEnzyme;
+  return {
+    panelEnzyme,
+    props
+  };
 }
 
 describe("Componenete Panel", () => {
   it("Debe renderizar componente", () => {
-    const panelEnzyme = crearPanel();
+    const { panelEnzyme } = crearPanel();
     expect(panelEnzyme.find("h1").text())
       .toBe("Panel pedidos");
   });
   it("La cantidad de elementos en la liste debe ser igual a los pedidos ", () => {
-    const panelEnzyme = crearPanel();
+    const { panelEnzyme } = crearPanel();
     expect(panelEnzyme.find("Pedido").length)
       .toBe(2);
+  });
+  it("Los pedidos se deben ordenar segun prioridad de manera descendente", () => {
+    const { panelEnzyme, props } = crearPanel();
+    const pedidos = panelEnzyme.find("Pedido");
+    const keyPrimerPedido = Number(pedidos.at(0).key());
+    const keySegundoPedido = Number(pedidos.at(1).key());
+    expect(keyPrimerPedido)
+      .toBe(props.pedidos[1].id);
+    expect(keySegundoPedido)
+      .toBe(props.pedidos[0].id);
+  });
+  it("Debe determinar cual de los pedidos tiene mayor prioridad", () => {
+    const { props } = crearPanel();
+    const pedidoPrioridadInferior = props.pedidos[0];
+    const pedidoPrioridadSuperior = props.pedidos[1];
+    expect(Panel.compararPrioridadPedidos(pedidoPrioridadInferior, pedidoPrioridadSuperior))
+      .toBeGreaterThanOrEqual(0);
+  });
+  it("Debe determinar cual de los pedidos tiene menor prioridad", () => {
+    const { props } = crearPanel();
+    const pedidoPrioridadInferior = props.pedidos[0];
+    const pedidoPrioridadSuperior = props.pedidos[1];
+    expect(Panel.compararPrioridadPedidos(pedidoPrioridadSuperior, pedidoPrioridadInferior))
+      .toBeLessThan(0);
   });
 });
