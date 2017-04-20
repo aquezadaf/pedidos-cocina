0fbfcfdf243384b9642d50bd30f5d044105881a8
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-04-20 01:44:53 -0300

    Se cambia el color del pedido segun su prioridad

--- app/components/Pedido.js
+++ app/components/Pedido.js
@@ -9,10 +9,29 @@ export default class Pedido extends Component {
     ordenes: Array
   }
 
+  clasePrioridadPedido() {
+    const coloresPrioridades = [
+      style.prioridadBaja,
+      style.prioridadNormal,
+      style.prioridadAlta,
+      style.prioridadMuyAlta
+    ];
+
+    let { prioridad } = this.props;
+    if (prioridad > 3) {
+      prioridad = 3;
+    } else if (prioridad < 0) {
+      prioridad = 0;
+    }
+
+    return [style.pedido, coloresPrioridades[prioridad]].join(" ");
+  }
+
   render() {
     const { nombre, fechaSolicitud, ordenes, prioridad } = this.props;
+    const nombreClasePedido = this.clasePrioridadPedido();
     return (
-      <div className={style.pedido}>
+      <div className={nombreClasePedido}>
         <div className={style.nombre}>
           ({prioridad}) {nombre}
         </div>
