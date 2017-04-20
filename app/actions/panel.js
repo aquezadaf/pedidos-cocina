d1caf5d93f8750b552b04e78081eadb13ab69fc3
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-04-19 23:48:22 -0300

    Se genera accion para aumentar la prioridad de los pedidos

--- app/actions/panel.js
+++ app/actions/panel.js
@@ -2,6 +2,7 @@ import { onPedidoNuevo, onPedidoFinalizado } from "../utils/socket";
 
 export const AGREGAR_PEDIDO = "AGREGAR_PEDIDO";
 export const ELIMINAR_PEDIDO = "ELIMINAR_PEDIDO";
+export const AUMENTAR_PRIORIDAD_PEDIDO = "AUMENTAR_PRIORIDAD_PEDIDO";
 
 export function agregarPedido(pedido) {
   return {
@@ -13,6 +14,13 @@ export function agregarPedido(pedido) {
 export function eliminarPedido(id) {
   return {
     type: ELIMINAR_PEDIDO,
+    id
+  };
+}
+
+export function aumentarPrioridadPedido(id) {
+  return {
+    type: AUMENTAR_PRIORIDAD_PEDIDO,
     id
   };
 }
