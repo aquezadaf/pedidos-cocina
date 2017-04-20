384481cfc0f92e12cd4f660f1eaed62271406e50
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-04-20 18:42:09 -0300

    Se eliminan mensajes de log

--- app/reducers/panel.js
+++ app/reducers/panel.js
@@ -8,10 +8,8 @@ export default function panel(state = [], action) {
   switch (action.type) {
     case AGREGAR_PEDIDO:
       if (state.find((pedido) => pedido.id === action.pedido.id)) {
-        console.log("REPETIDO");
         return state;
       }
-      console.log("NUEVO");
       return [
         ...state,
         action.pedido
