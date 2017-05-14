46ec5ff112c45e3e7c884bdc87b5d6237cd37290
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-14 17:39:24 -0400

    Se cambia el orden de las funciones y agrega comentario para describir el proposito del archivo

--- app/main.dev.js
+++ app/main.dev.js
@@ -1,47 +1,62 @@
+/* eslint global-require: 1, flowtype-errors/show-errors: 0 */
+
+/**
+ * This module executes inside of electron"s main process. You can start
+ * electron renderer process from here and communicate with the other processes
+ * through IPC.
+ *
+ * When running `npm run build` or `npm run build-main`, this file is compiled to
+ * `./app/main.prod.js` using webpack. This gives us some performance wins.
+ *
+ */
 import { app, BrowserWindow } from "electron";
 import MenuBuilder from "./menu";
 
 let mainWindow = null;
 
 if (process.env.NODE_ENV === "production") {
-  const sourceMapSupport = require("source-map-support"); // eslint-disable-line
+  const sourceMapSupport = require("source-map-support");
   sourceMapSupport.install();
 }
 
-if (process.env.NODE_ENV === "development") {
-  require("electron-debug")(); // eslint-disable-line global-require
-  const path = require("path"); // eslint-disable-line
-  const p = path.join(__dirname, "..", "app", "node_modules"); // eslint-disable-line
-  require("module").globalPaths.push(p); // eslint-disable-line
+if (process.env.NODE_ENV === "development" || process.env.DEBUG_PROD === "true") {
+  require("electron-debug")();
+  const path = require("path");
+  const p = path.join(__dirname, "..", "app", "node_modules");
+  require("module").globalPaths.push(p);
 }
 
+const installExtensions = async () => {
+  const installer = require("electron-devtools-installer");
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = [
+    "REACT_DEVELOPER_TOOLS",
+    "REDUX_DEVTOOLS"
+  ];
+
+  return Promise
+    .all(extensions.map(name => installer.default(installer[name], forceDownload)))
+    .catch(console.log);
+};
+
+
+/**
+ * Add event listeners...
+ */
+
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+  // Respect the OSX convention of having the application in memory even
+  // after all windows have been closed
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 
-const installExtensions = async () => {
-  if (process.env.NODE_ENV === "development") {
-    const installer = require("electron-devtools-installer"); // eslint-disable-line global-require
-
-    const extensions = [
-      "REACT_DEVELOPER_TOOLS",
-      "REDUX_DEVTOOLS"
-    ];
-
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-
-    // TODO: Use async interation statement.
-    //       Waiting on https://github.com/tc39/proposal-async-iteration
-    //       Promises will fail silently, which isn"t what we want in development
-    return Promise
-      .all(extensions.map(name => installer.default(installer[name], forceDownload)))
-      .catch(console.log);
-  }
-};
-
 app.on("ready", async () => {
-  await installExtensions();
+  if (process.env.NODE_ENV === "development" || process.env.DEBUG_PROD === "true") {
+    await installExtensions();
+  }
 
   mainWindow = new BrowserWindow({
     show: false,
@@ -51,6 +66,8 @@ app.on("ready", async () => {
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
 
+  // @TODO: Use "ready-to-show" event
+  //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
   mainWindow.webContents.on("did-finish-load", () => {
     if (!mainWindow) {
       throw new Error("\"mainWindow\" is not defined");
