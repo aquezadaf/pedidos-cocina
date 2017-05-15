363d825b118690b413eab10f5750a03d5090624c
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-15 00:44:33 -0400

    Se agregan iconos al menu

--- app/components/MenuNavegacion.js
+++ app/components/MenuNavegacion.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import style from "./MenuNavegacion.css";
 
+const elementosMenu = [
+  { texto: "Inicio", icono: "fa-home" },
+  { texto: "Pedidos", icono: "fa-male" },
+  { texto: "Reservas", icono: "fa-mobile-phone" },
+  { texto: "Menu", icono: "fa-cutlery" },
+  { texto: "Configuración", icono: "fa-gears" }
+];
+
 export default class MenuNavegacion extends Component {
+  static generarElementoLista(textoLink, iconoFontAwesome) {
+    const icono = `fa ${iconoFontAwesome} fa-2x`;
+    return (
+      <li className={style.elementoLista}>
+        <Link to="/" className={style.link}>
+          <i className={icono} aria-hidden="true" />
+          <div className={style.textoLink}>{textoLink}</div>
+        </Link>
+      </li>
+    );
+  }
+
   render() {
     return (
       <div className={style.menuNavegacion}>
         <ul className={style.listaMenu}>
-          <li>Inicio</li>
-          <li>Pedidos</li>
-          <li>Reservas</li>
-          <li>Menu</li>
-          <li>Configuración</li>
+          {
+            elementosMenu.map(((elementoMenu) => (
+              MenuNavegacion.generarElementoLista(elementoMenu.texto, elementoMenu.icono)
+            )))
+          }
         </ul>
       </div>
     );
