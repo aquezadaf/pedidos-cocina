be39fa8176baf42c6a95f61aacad700ae93f74b3
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-15 03:44:19 -0400

    Se agregan pruebas de interaccion con el menu

--- test/components/MenuNavegacion.spec.js
+++ test/components/MenuNavegacion.spec.js
@@ -20,4 +20,22 @@ describe("Componente del Menu de Navegacion", () => {
     expect(menuEnzyme.find(".elementoListaSeleccionado").length)
       .toBe(1);
   });
+  it("Al hacer click en un menu no seleccionado se debe seleccionar", () => {
+    const menuEnzyme = crearMenuNavegacion();
+    const menuNoSeleccionado = menuEnzyme.find("Link").first();
+    const textoMenuNoSeleccionado = menuNoSeleccionado.find("div").text();
+    menuNoSeleccionado.simulate("click");
+    const textoMenuSeleccionado = menuEnzyme.find(".elementoListaSeleccionado").find(".textoLink").text();
+    expect(textoMenuSeleccionado)
+      .toEqual(textoMenuNoSeleccionado);
+  });
+  it("Al hacer click en un menu seleccionado se debe mantener seleccionado el mismo menu", () => {
+    const menuEnzyme = crearMenuNavegacion();
+    const menuSeleccionado = menuEnzyme.find(".elementoListaSeleccionado");
+    const textoMenuSeleccionado = menuSeleccionado.text();
+    menuSeleccionado.simulate("click");
+    const textoNuevoMenuSeleccionado = menuEnzyme.find(".elementoListaSeleccionado").find(".textoLink").text();
+    expect(textoNuevoMenuSeleccionado)
+      .toEqual(textoMenuSeleccionado);
+  });
 });
