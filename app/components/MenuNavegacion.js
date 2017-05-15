bf0e3fcfa788082a1227490c9694c74b6e4cba9f
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-14 21:33:51 -0400

    Se generan los archivos del nuevo componente

---
+++ app/components/MenuNavegacion.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import style from "./MenuNavegacion.css";
+
+export default class MenuNavegacion extends Component {
+  render() {
+    return (<div>Menu navegacion</div>);
+  }
+}
