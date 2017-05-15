6471493632c56a73172aac5b4005b7574aef6afc
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-15 03:24:08 -0400

    Se generan pruebas para app

--- test/components/App.spec.js
+++ test/components/App.spec.js
@@ -1,5 +1,27 @@
+import { shallow } from "enzyme";
+import React from "react";
+import App from "../../app/components/App";
+
+const crearApp = () => shallow((
+  <App>
+    <div>Children App</div>
+  </App>
+));
+
 describe("Componente de la App", () => {
   it("Debe renderizar componente", () => {
-    expect(true).toBe(false);
+    const appEnzyme = crearApp();
+    expect(appEnzyme.find("div").first().hasClass("app"))
+      .toBe(true);
+  });
+  it("Debe contener menu de navegacion", () => {
+    const appEnzyme = crearApp();
+    expect(appEnzyme.find("MenuNavegacion").exists())
+      .toBe(true);
+  });
+  it("Debe contener children", () => {
+    const appEnzyme = crearApp();
+    expect(appEnzyme.find("div").last().text())
+      .toBe("Children App");
   });
 });
