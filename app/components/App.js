3730cb9cdd73abc199d7716d15a97e896e91e023
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-14 23:11:27 -0400

    Se alininean los componentes de la aplicacion

--- app/components/App.js
+++ app/components/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MenuNavegacion from "./MenuNavegacion";
+import style from "./App.css";
 
 const propTypes = {
   children: PropTypes.element.isRequired
 };
 
 const App = ({ children }) => (
-  <div>
-    <MenuNavegacion />
-    <div>
+  <div className={style.app}>
+    <div className={style.menu}>
+      <MenuNavegacion />
+    </div>
+    <div className={style.contenido}>
       {children}
     </div>
   </div>
