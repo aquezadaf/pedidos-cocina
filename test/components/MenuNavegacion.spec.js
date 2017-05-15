8df4ca606af5beba775491ee4bc1a2e911e65f30
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-15 03:15:30 -0400

    Se generan las pruebas del menu de navegacion

--- test/components/MenuNavegacion.spec.js
+++ test/components/MenuNavegacion.spec.js
@@ -1,5 +1,23 @@
+import { shallow } from "enzyme";
+import React from "react";
+import MenuNavegacion from "../../app/components/MenuNavegacion";
+
+const crearMenuNavegacion = () => shallow(<MenuNavegacion />);
+
 describe("Componente del Menu de Navegacion", () => {
   it("Debe renderizar componente", () => {
-    expect(true).toBe(false);
+    const menuEnzyme = crearMenuNavegacion();
+    expect(menuEnzyme.find("div").first().hasClass("menuNavegacion"))
+      .toBe(true);
+  });
+  it("Menu debe contener cinco opciones", () => {
+    const menuEnzyme = crearMenuNavegacion();
+    expect(menuEnzyme.find("li").length)
+      .toBe(5);
+  });
+  it("Una de las opciones del menu debe estar seleccionada", () => {
+    const menuEnzyme = crearMenuNavegacion();
+    expect(menuEnzyme.find(".elementoListaSeleccionado").length)
+      .toBe(1);
   });
 });
