083b272137b89056abbfa1e2513e2653d152ccfd
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-15 19:20:50 -0400

    Se marca menu como seleccionado utilizando funcionalidad de NavLink

--- app/components/MenuNavegacion.js
+++ app/components/MenuNavegacion.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import style from "./MenuNavegacion.css";
 
 const elementosMenu = [
@@ -11,52 +11,24 @@ const elementosMenu = [
 ];
 
 export default class MenuNavegacion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { menuSeleccionado: "fa-home" };
-  }
-
-  seleccionarMenu(menuSeleccionado) {
-    this.setState({ menuSeleccionado });
-  }
-
-  generarElementoMenu({ path, texto, icono }) {
-    const claseElementoMenu = this.state.menuSeleccionado === icono
-      ? style.elementoListaSeleccionado
-      : style.elementoLista;
-    return (
-      <li key={icono} className={claseElementoMenu}>
-        {
-          this.generarLink(path, texto, icono)
-        }
-      </li>
-    );
-  }
-
-  generarLink(path, texto, icono) {
-    const claseIcono = `fa ${icono} fa-2x`;
-    if (this.state.menuSeleccionado === icono) {
-      return (
-        <div className={style.link}>
-          <i className={claseIcono} aria-hidden="true" />
-          <div className={style.textoLink}>{texto}</div>
-        </div>
-      );
-    }
-    return (
-      <Link to={path} className={style.link} onClick={this.seleccionarMenu.bind(this, icono)}>
-        <i className={claseIcono} aria-hidden="true" />
-        <div className={style.textoLink}>{texto}</div>
-      </Link>
-    );
-  }
-
   render() {
     return (
       <div className={style.menuNavegacion}>
         <ul className={style.listaMenu}>
           {
-            elementosMenu.map(((elementoMenu) => this.generarElementoMenu(elementoMenu)))
+            elementosMenu.map(({ path, icono, texto }) => {
+              const claseIcono = `fa ${icono} fa-2x`;
+              return (
+                <li key={path} className={style.elementoLista}>
+                  <NavLink
+                    exact to={path} className={style.link} activeClassName={style.linkActivo}
+                  >
+                    <i className={claseIcono} aria-hidden="true" />
+                    <div className={style.textoLink}>{texto}</div>
+                  </NavLink>
+                </li>
+              );
+            })
           }
         </ul>
       </div>
