9a72b3457215dbe9d365f31341145e87a186eca3
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-16 12:10:27 -0400

    Se agrega menu del restaurante

---
+++ app/components/MenuRestaurante.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class MenuRestaurante extends Component {
+  render() {
+    return (
+      <div>Menu del restaurante</div>
+    );
+  }
+}
---
+++ app/containers/MenuRestaurantePage.js
@@ -0,0 +1,4 @@
+import { connect } from "react-redux";
+import MenuRestaurante from "../components/MenuRestaurante";
+
+export default connect()(MenuRestaurante);
