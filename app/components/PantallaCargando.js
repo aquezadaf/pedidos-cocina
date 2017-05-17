58ab6f2d43450dcdc79206875894e7b53abb148d
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-16 20:18:35 -0400

    Pantalla cargando muestra un componente de manera condicional si esta ccargando

--- app/components/PantallaCargando.js
+++ app/components/PantallaCargando.js
@@ -1,11 +1,16 @@
 import React from "react";
-import style from "./Spinner.css";
+import style from "./PantallaCargando.css";
 
-export default () => (
-  <div className={style.spinner}>
-    <div>
-      <i className="fa fa-refresh fa-spin fa-3x fa-fw" />
-    </div>
-    <div className={style.texto}>Cargando...</div>
-  </div>
-);
+export default ({ estaCargando, children }) => {
+  if (estaCargando) {
+    return (
+      <div className={style.spinner}>
+        <div>
+          <i className="fa fa-refresh fa-spin fa-3x fa-fw" />
+        </div>
+        <div className={style.texto}>Cargando...</div>
+      </div>
+    );
+  }
+  return children;
+};
