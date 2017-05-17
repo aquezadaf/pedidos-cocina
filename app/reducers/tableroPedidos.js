a1a73feca9edd0b72d7134fc4efb7f083fc5599c
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-17 15:41:44 -0400

    Se agrega logica de carga de pedidos y estadoo cargando

--- app/reducers/tableroPedidos.js
+++ app/reducers/tableroPedidos.js
@@ -2,33 +2,56 @@ import {
   AGREGAR_PEDIDO,
   ELIMINAR_PEDIDO,
   AUMENTAR_PRIORIDAD_PEDIDO,
+  SOLICITAR_PEDIDOS,
   CARGAR_PEDIDOS
 } from "../actions/tableroPedidos";
 
-export default (state = [], action) => {
+const estadoInicial = {
+  pedidos: [],
+  estaCargando: false
+};
+
+export default (state = estadoInicial, action) => {
   switch (action.type) {
     case AGREGAR_PEDIDO:
-      if (state.find((pedido) => pedido.id === action.pedido.id)) {
+      if (state.pedidos.find((pedido) => pedido.id === action.pedido.id)) {
         return state;
       }
-      return [
+      return {
         ...state,
-        action.pedido
-      ];
+        pedidos: [
+          ...state.pedidos,
+          action.pedido
+        ]
+      };
     case ELIMINAR_PEDIDO:
-      return state.filter((pedido) => pedido.id !== action.id);
+      return {
+        ...state,
+        pedidos: state.pedidos.filter((pedido) => pedido.id !== action.id)
+      };
     case AUMENTAR_PRIORIDAD_PEDIDO:
-      return state.map((pedido) => {
-        if (pedido.id !== action.id) {
-          return pedido;
-        }
-        return {
-          ...pedido,
-          prioridad: pedido.prioridad + 1
-        };
-      });
+      return {
+        ...state,
+        pedidos: state.pedidos.map((pedido) => {
+          if (pedido.id !== action.id) {
+            return pedido;
+          }
+          return {
+            ...pedido,
+            prioridad: pedido.prioridad + 1
+          };
+        })
+      };
+    case SOLICITAR_PEDIDOS:
+      return {
+        ...state,
+        estaCargando: true
+      };
     case CARGAR_PEDIDOS:
-      return action.pedidos;
+      return {
+        pedidos: action.pedidos,
+        estaCargando: false
+      };
     default:
       return state;
   }
