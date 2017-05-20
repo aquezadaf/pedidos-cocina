368833cbcd92da84079d183fbde6769c2b7b6d08
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-20 05:23:29 -0400

    Se cambian pruebas de la pantalla para incluir el conector

--- test/components/PantallaCargando.spec.js
+++ test/components/PantallaCargando.spec.js
@@ -1,14 +1,15 @@
 import { shallow } from "enzyme";
 import React from "react";
-import PantallaCargando from "../../app/components/PantallaCargando";
+import PantallaCargando, { conectarPantallaCargando } from "../../app/components/PantallaCargando";
 
 const crearPantallaCargando = (estaCargando) => {
-  const props = {
-    estaCargando,
-    children: <div>Componente hijo</div>
-  };
+  const children = <div>Componente hijo</div>;
 
-  return shallow(<PantallaCargando {...props} />);
+  return shallow(
+    <PantallaCargando estaCargando={estaCargando}>
+      {children}
+    </PantallaCargando>
+  );
 };
 
 describe("Componente Pantalla Cargando", () => {
@@ -21,5 +22,30 @@ describe("Componente Pantalla Cargando", () => {
     const pantallaEnzyme = crearPantallaCargando(true);
     expect(pantallaEnzyme.find(".texto").text())
       .toBe("Cargando...");
+  });
+});
+
+describe("Conector Pantalla Cargando", () => {
+  const crearComponeneteConectado = (estaCargando) => {
+    const componente = () => (<div>Componente a conectar</div>);
+    const ComponenteConectado = conectarPantallaCargando(componente)({ estaCargando });
+    return shallow(ComponenteConectado);
+  };
+
+  it("Debe generar funcion al recibir el componente a conectar", () => {
+    const componente = () => { };
+    const funcionPropiedades = conectarPantallaCargando(componente);
+    expect(typeof funcionPropiedades)
+      .toBe("function");
+  });
+  it("Al ejecutar funcion retornada por el conector, si la pantalla esta cargando retornar el componente conectado", () => {
+    const componenteConectado = crearComponeneteConectado(false);
+    expect(componenteConectado.text())
+      .toEqual("<componente />");
+  });
+  it("Al ejecutar funcion retornada por el conector, si esta cargando debe retornar texto cargando", () => {
+    const componenteConectado = crearComponeneteConectado(true);
+    expect(componenteConectado.text())
+      .toEqual("Cargando...");
   });
 });
