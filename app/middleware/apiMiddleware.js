372c7becd5a1f68f19cc441e74a2c92cb7f32fb1
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-05-20 21:25:28 -0400

    Se termina middleware de api ysus pruebas

--- app/middleware/apiMiddleware.js
+++ app/middleware/apiMiddleware.js
@@ -1,19 +1,36 @@
 export const LLAMAR_API = "LLAMAR_API";
 
-const validarPropiedadesAction = (propiedadesAction) => {
-  if (!propiedadesAction.types || propiedadesAction.types.length !== 3) {
+const validarPropiedadesApi = (propiedadesApi) => {
+  if (!propiedadesApi.types || propiedadesApi.types.length !== 3) {
     throw new Error("Types debe contener 3 elementos");
   }
-  if (!propiedadesAction.endpoint) {
+  if (!propiedadesApi.endpoint) {
     throw new Error("Se debe enviar un endpoint para consumir la api");
   }
-};
-
-const apiMiddleware = urlApi => ({ dispatch }) => next => action => {
-  if (!action || !action[LLAMAR_API]) {
-    return next(action);
+  if (!propiedadesApi.nombrePayloadFetch) {
+    throw new Error("Se debe especificar un nombre para el payload de los resultados de fetch");
   }
-  validarPropiedadesAction(action[LLAMAR_API]);
 };
 
+const apiMiddleware = (urlApi) => {
+  if (!urlApi) {
+    throw new Error("Se debe especificar una url para consumir la api");
+  }
+  return () => (next) => (action) => {
+    if (!action || !action[LLAMAR_API]) {
+      return next(action);
+    }
+    const propiedadesApi = action[LLAMAR_API];
+    validarPropiedadesApi(propiedadesApi);
+
+    const { types, endpoint, nombrePayloadFetch } = propiedadesApi;
+    const [INICIO_LLAMADA_API, LLAMADA_API_FINALIZADA, ERROR_LLAMADA_API] = types;
+
+    next({ type: INICIO_LLAMADA_API });
+    return fetch(`${urlApi}${endpoint}`)
+      .then(response => response.json())
+      .then(response => next({ type: LLAMADA_API_FINALIZADA, [nombrePayloadFetch]: response }))
+      .catch(error => next({ type: ERROR_LLAMADA_API, error }));
+  };
+};
 export default apiMiddleware;
--- test/middleware/apiMiddleware.spec.js
+++ test/middleware/apiMiddleware.spec.js
@@ -1,31 +1,97 @@
 import apiMiddleware, { LLAMAR_API } from "../../app/middleware/apiMiddleware";
 
 const ejecutarAction = (action) => {
-  const urlApi = "";
+  const urlApi = "http://localhost/";
   const middleware = apiMiddleware(urlApi);
-  const dispatch = { dispatch: jest.fn() };
   const next = jest.fn();
-  middleware(dispatch)(next)(action);
+  const promesaMiddleware = middleware()(next)(action);
   return {
-    llamadasDispatch: dispatch.dispatch.mock.calls,
-    llamadasNext: next.mock.calls
+    llamadasNext: next.mock.calls,
+    promesaMiddleware
+  };
+};
+
+const accionApiValida = {
+  [LLAMAR_API]: {
+    types: ["PRIMERO", "SEGUNDO", "TERCERO"],
+    endpoint: "pedidos",
+    nombrePayloadFetch: "pedidos"
+  }
+};
+
+const mockFetchAccionApiValida = () => {
+  const respuestaFetch = [
+    { id: 1, nombre: "Primer pedido" },
+    { id: 2, nombre: "Segundo pedido" }
+  ];
+  const respuestaMock = fetch.mockResponseOnce(JSON.stringify(respuestaFetch));
+  return {
+    respuestaFetch,
+    respuestaMock
   };
 };
 
 describe("Api Middleware", () => {
-  it("Si no contiene propiedad con el nombre LLAMAR_API debe ejecutar next", () => {
-    const { llamadasNext } = ejecutarAction({ type: "LLAMAR_API", otraPropiedad: "Valor propiedad" });
+  it("Debe arrojar error si no se pasa una url al momento de generar el middleware", () => {
+    expect(() => {
+      apiMiddleware();
+    }).toThrow(/url/);
+  });
+  it("Si no contiene propiedad con el nombre LLAMAR_API debe ejecutar next con el action recibido", () => {
+    const actionSinLlamadaApi = { type: "LLAMAR_API", otraPropiedad: "Valor propiedad" };
+    const { llamadasNext } = ejecutarAction(actionSinLlamadaApi);
     expect(llamadasNext.length)
       .toBe(1);
+    expect(llamadasNext[0][0])
+      .toBe(actionSinLlamadaApi);
   });
   it("Debe arrojar error si types no contiene tres elementos", () => {
     expect(() => {
-      ejecutarAction({ [LLAMAR_API]: { types: [1, 2], endpoint: "" } });
+      ejecutarAction({ [LLAMAR_API]: { types: ["PRIMERO", "SEGUNDO"], endpoint: "pedidos" } });
     }).toThrow(/Types/);
   });
   it("Debe arrojar error si no contiene endpoint de la api", () => {
     expect(() => {
-      ejecutarAction({ [LLAMAR_API]: { types: [1, 2, 3] } });
+      ejecutarAction({ [LLAMAR_API]: { types: ["PRIMERO", "SEGUNDO", "TERCERO"] } });
     }).toThrow(/endpoint/);
+  });
+  it("Debe arrojar error si no contiene el nombre del payload de retorno de la api", () => {
+    expect(() => {
+      ejecutarAction({ [LLAMAR_API]: { types: ["PRIMERO", "SEGUNDO", "TERCERO"], endpoint: "pedidos" } });
+    }).toThrow(/payload/);
+  });
+  it("Antes de realizar consulta a la api, debe ejecutar next del primer tipo en types", () => {
+    const { llamadasNext } = ejecutarAction(accionApiValida);
+    const parametroNext = llamadasNext[0][0];
+    expect(parametroNext)
+      .toEqual({ type: accionApiValida[LLAMAR_API].types[0] });
+  });
+  it("Despues de finalizar satisfactoriamente la consulta a la api, debe ejecutar next del segunto tipo en types", () => {
+    const { respuestaFetch } = mockFetchAccionApiValida();
+    const { llamadasNext, promesaMiddleware } = ejecutarAction(accionApiValida);
+    return promesaMiddleware
+      .then(() => {
+        const parametroNext = llamadasNext[1][0];
+        return expect(parametroNext)
+          .toEqual({ type: accionApiValida[LLAMAR_API].types[1], pedidos: respuestaFetch });
+      });
+  });
+  it("Si la consulta a la api finaliza con errrores, debe ejecutar next del tercer tipo en types", () => {
+    const { llamadasNext, promesaMiddleware } = ejecutarAction(accionApiValida);
+    return promesaMiddleware
+      .then(() => {
+        const parametroNext = llamadasNext[1][0];
+        expect(parametroNext.type)
+          .toEqual(accionApiValida[LLAMAR_API].types[2]);
+        return expect(parametroNext.error)
+          .toBeTruthy();
+      });
+  });
+  it("La url que se consulta en fetch debe ser el endpoint concatenado con la url de configuracion del middleware", () => {
+    const { respuestaMock } = mockFetchAccionApiValida();
+    const { promesaMiddleware } = ejecutarAction(accionApiValida);
+    return promesaMiddleware
+      .then(() => expect(respuestaMock)
+        .toHaveBeenCalledWith("http://localhost/pedidos"));
   });
 });
