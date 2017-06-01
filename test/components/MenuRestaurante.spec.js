5183ad780f9316eec316442797c29a4d149b033e
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-06-01 02:12:00 -0400

    Se corriguen pruebas y agrega flow

--- test/components/MenuRestaurante.spec.js
+++ test/components/MenuRestaurante.spec.js
@@ -1,3 +1,4 @@
+// @flow
 import { mount } from "enzyme";
 import React from "react";
 import MenuRestaurante from "../../app/components/MenuRestaurante";
@@ -5,9 +6,9 @@ import MenuRestaurante from "../../app/components/MenuRestaurante";
 const crearMenuRestaurante = () => {
   const props = {
     platosMenu: [
-      "Primer plato",
-      "Segundo plato",
-      "Tercer plato"
+      { id: 1, nombre: "Primer plato", descripcion: "Plato", precio: 1000, urlFotoPlato: "", tiempoAproximadoPreparacion: 10 },
+      { id: 2, nombre: "Segundo plato", descripcion: "Plato", precio: 1000, urlFotoPlato: "", tiempoAproximadoPreparacion: 10 },
+      { id: 3, nombre: "Tercer plato", descripcion: "Plato", precio: 100, urlFotoPlato: "", tiempoAproximadoPreparacion: 10 }
     ],
     solicitarMenuRestaurante: jest.fn()
   };
@@ -23,12 +24,12 @@ const crearMenuRestaurante = () => {
 describe("Componente Menu Restaurante", () => {
   it("Debe renderizar componente", () => {
     const { menuEnzyme } = crearMenuRestaurante();
-    expect(menuEnzyme.find("div > ul").exists())
+    expect(menuEnzyme.find("div").first().hasClass("menu"))
       .toBe(true);
   });
   it("Debe mostrar todos los platos del menu", () => {
     const { menuEnzyme, props } = crearMenuRestaurante();
-    const platosEnComponente = menuEnzyme.find("div > ul > li").length;
+    const platosEnComponente = menuEnzyme.find("div > div > h4").length;
     expect(platosEnComponente)
       .toBe(props.platosMenu.length);
   });
