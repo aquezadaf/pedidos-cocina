82b2b4dc93b40bbf9e2d20c90dc1b43a6941bf79
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-06-01 00:04:51 -0400

    Se agrega detale del plato

--- app/components/MenuRestaurante.js
+++ app/components/MenuRestaurante.js
@@ -1,9 +1,17 @@
 // @flow
 import React, { Component } from "react";
+import style from "./MenuRestaurante.css";
 
 export default class MenuRestaurante extends Component {
   props: {
-    platosMenu: Array<string>,
+    platosMenu: Array<{
+      id: number,
+      nombre: string,
+      descripcion: string,
+      precio: number,
+      tiempoAproximadoPreparacion: number,
+      urlFotoPlato: string
+    }>,
     solicitarMenuRestaurante: () => {}
   }
 
@@ -14,14 +22,34 @@ export default class MenuRestaurante extends Component {
   render() {
     const { platosMenu } = this.props;
     return (
-      <div>
-        <ul>
-          {platosMenu.map((platoMenu) => (
-            <li key={platoMenu}>
-              {platoMenu}
-            </li>
-          ))}
-        </ul>
+      <div className={style.menu}>
+        {
+          platosMenu.map((platoMenu) => (
+            <div key={platoMenu.id} className={style.platoMenu}>
+              <h4 className={style.tituloPlatoMenu}>
+                {platoMenu.nombre}
+              </h4>
+              <div className={style.contenidoPlatoMenu}>
+                <div className={style.contenedorFoto}>
+                  <img
+                    className={style.fotoPlato}
+                    src={platoMenu.urlFotoPlato}
+                    alt={platoMenu.nombre}
+                  />
+                </div>
+                <div>
+                  {platoMenu.descripcion}
+                </div>
+                <div>
+                  Precio: $ {platoMenu.precio}
+                </div>
+                <div>
+                  Tiempo preparacion: {platoMenu.tiempoAproximadoPreparacion} minutos
+                </div>
+              </div>
+            </div>
+          ))
+        }
       </div>
     );
   }
