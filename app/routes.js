45e2088024e5c21e19cef132b35d8ae1ea0e6963
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-08-28 19:43:51 -0300

    Agrega ruta reservas

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from "react-router";
 import App from "./components/App";
 import TableroPedidosPage from "./containers/TableroPedidosPage";
 import MenuRestaurantePage from "./containers/MenuRestaurantePage";
+import TableroReservasPage from "./containers/TableroReservasPage";
 
 export default () => (
   <App>
     <Switch>
       <Route exact path="/" component={TableroPedidosPage} />
+      <Route exact path="/reservas" component={TableroReservasPage} />
       <Route exact path="/menu" component={MenuRestaurantePage} />
     </Switch>
   </App>
