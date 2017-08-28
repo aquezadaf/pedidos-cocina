7b2e6e3da1258375e0fb9995b9f66d5ef0b37489
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-08-28 20:03:54 -0300

    Agrega acciones de las reservas

---
+++ app/actions/tableroReservas.js
@@ -0,0 +1,34 @@
+import { LLAMAR_API } from "../middleware/apiMiddleware";
+
+export const SOLICITAR_RESERVAS = "SOLICITAR_RESERVAS";
+export const CARGAR_RESERVAS = "CARGAR_RESERVAS";
+export const ERROR_SOLICITUD_RESERVAS = "ERROR_SOLICITUD_RESERVAS";
+
+const cambiarTipoFechaSolicitud = (reserva) => {
+  if (typeof reserva.fechaSolicitud === "string") {
+    return {
+      ...reserva,
+      fechaSolicitud: new Date(reserva.fechaSolicitud)
+    };
+  }
+  return reserva;
+};
+
+const debeObtenerReservas = ({ tableroReservas }) => {
+  if (tableroReservas.estaCargando) {
+    return false;
+  } else if (tableroReservas.reservas && tableroReservas.reservas.length > 0) {
+    return false;
+  }
+  return true;
+};
+
+export const solicitarPedidos = () => ({
+  [LLAMAR_API]: {
+    types: [SOLICITAR_RESERVAS, CARGAR_RESERVAS, ERROR_SOLICITUD_RESERVAS],
+    endpoint: "reservas",
+    nombrePayloadFetch: "reservas",
+    llamarApi: debeObtenerReservas,
+    procesarResultados: (reservas) => reservas.map(cambiarTipoFechaSolicitud)
+  }
+});
--- app/containers/TableroReservasPage.js
+++ app/containers/TableroReservasPage.js
@@ -1,16 +1,16 @@
-// import { bindActionCreators } from "redux";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import TableroReservas from "../components/TableroReservas";
 import { conectarPantallaCargando } from "../components/PantallaCargando";
-// import * as accionesMenuRestaurante from "../actions/menuRestaurante";
+import * as accionesTableroReservas from "../actions/tableroReservas";
 
 // const mapStateToProps = ({ menuRestaurante }) => ({
 //   platosMenu: menuRestaurante.platosMenu,
 //   estaCargando: menuRestaurante.estaCargando
 // });
 
-// const mapDispatchToProps = (dispatch) => bindActionCreators(accionesMenuRestaurante, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators(accionesTableroReservas, dispatch);
 
 const reservasConCargando = conectarPantallaCargando(TableroReservas);
 
-export default connect(null, null)(reservasConCargando);
+export default connect(null, mapDispatchToProps)(reservasConCargando);
