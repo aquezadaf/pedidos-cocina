4dc0958707822e70de05e206e234686c7435bebd
Alejandro Quezada <alejandro_quezada92@hotmail.com>
2017-09-04 15:36:39 -0300

    Se agrega redireccion al presionar inicio sesion

--- app/components/FormularioInicioSesion.js
+++ app/components/FormularioInicioSesion.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 import style from "./FormularioInicioSesion.css";
 
-export default () => (
-  <div className={style.box}>
-    <div className={style.formulario}>
-      <div className={style.tag}>Reserva Ya!</div>
-      <div>Inicia sesión para configurar tu cocina</div>
-      <div>
-        <input type="text" id="inputName" placeholder="Usuario" />
+export default class FormularioInicioSesion extends Component {
+  state: { inicioSesion: boolean };
+
+  constructor() {
+    super();
+    this.state = { inicioSesion: false };
+  }
+
+  iniciarSesion = () => {
+    this.setState({ inicioSesion: true });
+  };
+
+  render() {
+    const { inicioSesion } = this.state;
+    if (inicioSesion) {
+      return <Redirect to={"/restaurante"} />;
+    }
+
+    return (
+      <div className={style.box}>
+        <div className={style.formulario}>
+          <div className={style.tag}>Reserva Ya!</div>
+          <div>Inicia sesión para configurar tu cocina</div>
+          <div>
+            <input type="text" id="inputName" placeholder="Usuario" />
+          </div>
+          <div>
+            <input type="password" id="inputPassword" placeholder="Contraseña" />
+          </div>
+          <div>
+            <button type="submit" className={style.btn} onClick={this.iniciarSesion}>
+              Iniciar sesión
+            </button>
+          </div>
+        </div>
       </div>
-      <div>
-        <input type="password" id="inputPassword" placeholder="Contraseña" />
-      </div>
-      <div>
-        <button type="submit" className={style.btn}>
-            Iniciar sesión
-          </button>
-      </div>
-    </div>
-  </div>
-  );
+    );
+  }
+}
